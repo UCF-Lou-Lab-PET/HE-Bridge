af9c7a18234351d518819d474aaf92eeec35a88f
fffobow <fffobow@gmail.com>
2024-11-07 22:25:06 -0500

    script update

---
+++ src/run.sh
@@ -0,0 +1,9 @@
+# ./bin/bridge_circuit p=5 r=4 m=16151 b=320 t=64
+
+# ./bin/bridge_circuit p=17 r=2 m=13201 b=256 t=64
+
+# ./bin/bridge_circuit p=67 r=2 m=19801 b=413 t=64
+
+# ./bin/bridge_circuit p=257 r=2 m=37837 b=768 t=64
+
+./bin/bridge_circuit p=31 r=4 m=42407 b=879 t=64
